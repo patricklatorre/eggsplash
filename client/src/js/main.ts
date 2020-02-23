045311de9390bcb00b9c501132eb305797467d1d
Patrick Latorre <ptrckltrr@gmail.com>
2020-02-23 23:47:02 +0800

    client: separate ts to modules

--- client/src/js/main.ts
+++ client/src/js/main.ts
@@ -1,14 +1,4 @@
-async function getImages(page: number) {
-  const url = `http://localhost:9090/photo?page=${page}`
-  const response = await fetch(url, {
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer'
-  })
+import * as _ from './util'
+import * as photos from './photos'
 
-  return await response.json(); // Parse json
-}
 
