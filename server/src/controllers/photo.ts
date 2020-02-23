9480f85710620a1597c8a60efec6f7d5c5dca64e
Patrick Latorre <ptrckltrr@gmail.com>
2020-02-23 21:14:48 +0800

    Update unsplash search key to "eggs"

--- server/src/controllers/photo.ts
+++ server/src/controllers/photo.ts
@@ -3,21 +3,21 @@ import Unsplash, { toJson } from 'unsplash-js'
 import config from '../config'
 import { Request, Response } from 'express'
 
+// Initialize unsplash lib
 const unsplash = new Unsplash({ accessKey: config.unsplash.accessKey })
 
 const validatePage = async (page: number) => {
-  if (page && !isNaN(page) && page > 0) {
+  if (page && !isNaN(page) && page > 0)
     return page
-  } else {
+  else
     return null
-  }
 }
 
 export const index = async (req: Request, res: Response) => {
   const page = await validatePage(req.query.page as number) || 1
   const picsPerPage = 3
 
-  unsplash.search.photos('egg', page, picsPerPage)
+  unsplash.search.photos('eggs', page, picsPerPage)
     .then(toJson)
     .then(json => {
       res.json(json)
